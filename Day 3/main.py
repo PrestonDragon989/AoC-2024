154bd59c1d0a0b85fc7cdd40bc845feb2e0d06eb
Preston E <122578013+PrestonDragon989@users.noreply.github.com>
2024-12-14 00:28:20 -0800

    Part 1 Complete
    Finished final touches on input parser (nested muls, fixed return types, better regexs/statements) & added part 1 implementation and completion in main.

--- Day 3/main.py
+++ Day 3/main.py
@@ -8,8 +8,21 @@ from parser import InputParser
 def main():
     """
     Main function for the Day 3 AoC challenge. First collects the mul_values from the input file with InputParser.
+
+    Part 1:
+        iterating through the mul values, then multiplying the two numbers together and adding them to a total.
     """
-    mul_values: list[set[int, int]] = InputParser("input.txt").get_mul_numbers()
+    mul_values: list[[int, int]] = InputParser("input.txt").get_mul_numbers()
+
+    print(mul_values)
+    print(len(mul_values))
+
+    # Part 1
+    total_mul: float = 0
+    for mul_parameters in mul_values:
+        total_mul += mul_parameters[0] * mul_parameters[1]
+
+    print(f"Total Mul: {total_mul}")
 
 
 if __name__ == '__main__':
--- Day 3/parser.py
+++ Day 3/parser.py
@@ -12,7 +12,7 @@ class InputParser:
     def __init__(self, file_path: str) -> None:
         self._file_path: str = file_path
 
-    def _valid_mul_string(self, mul_string: str) -> bool or set[int, int]:
+    def _valid_mul_string(self, mul_string: str) -> bool or list[int, int]:
         """
         Checks to make sure the given string is valid for mul(x,x). It first parses the numbers of the expression, then
         makes sure that what it got was correct. Then it collects the numbers to return.
@@ -22,22 +22,19 @@ class InputParser:
         if self._filler_character in mul_string:
             return False
 
-        # Same string as mul_string, just replacing all numbers with 'x'
-        try:
-            num_removed_string: str = mul_string.format(
-                lambda letter: letter if letter not in self._string_numbers else self._filler_character)
-        except ValueError:
-            return False  # This just means it's an invalid string and responds badly
+        # Regex: Replaces all numbers with the filler character (_filler_character)
+        num_removed_string = re.sub(r'\d', self._filler_character, mul_string)
 
         # Regex: takes all clusters of x that are above on and turns them to one. (ex: "xx fds j xxx" => "x fds j x"
         # Used to simplify the mul expression to check if it matches the placeholder one (_base_mul_string)
         num_removed_string: str = re.sub(f'({self._filler_character})+', lambda m: m.group(1), num_removed_string)
         if num_removed_string != self._base_mul_string:
+            # print(mul_string)
             return False
 
         else:
             split_mul: list[str] = mul_string.split(",")
-            return int(split_mul[0].split("(", 1)[1]), int(split_mul[1].split(")", 1)[0])
+            return [int(split_mul[0].split("(", 1)[1]), int(split_mul[1].split(")", 1)[0])]
 
     @staticmethod
     def _reset_input_string_mul_start(file_text) -> str or False:
@@ -52,14 +49,14 @@ class InputParser:
         except ValueError:
             return False
 
-    def get_mul_numbers(self) -> list[set[int, int]]:
+    def get_mul_numbers(self) -> list[list[int, int]]:
         """
         Scans the contents of the given file for all instances of mul(x,x), using _reset_input_string_mul_start and
         _valid_mul_string to reset and check.
         @return: A list of number sets to multiply, taken from the input file
         """
-        mul_numbers: list[set[int, int]] = []
-        mul_raw_string: list[str] = []
+        mul_numbers: list[list[int, int]] = []
+        mul_raw_strings: list[str] = []
 
         with open(self._file_path, "r") as f:
             file_text: str or bool = self._reset_input_string_mul_start(f.read().replace("\n", ""))
@@ -70,8 +67,15 @@ class InputParser:
                 try:
                     mul_index: int = file_text.index("mul(")
                     end_index: int = file_text.index(")")
+                    mul_string: str = file_text[mul_index:end_index + 1]
 
-                    mul_raw_string.append(file_text[mul_index:end_index + 1])
+                    mul_raw_strings.append(mul_string)
+
+                    # Checking correct mul statements in found ones
+                    if mul_string.count("mul(") > 1:
+                        mul_raw_strings.pop(-1)
+                        file_text = self._reset_input_string_mul_start(file_text[mul_index + 1:])
+                        continue
 
                     file_text = self._reset_input_string_mul_start(file_text[end_index - 1:])
                     if not file_text:
@@ -80,8 +84,9 @@ class InputParser:
                 except ValueError:
                     break
 
-        for raw_string in mul_raw_string:
-            valid: bool or set[int, int] = self._valid_mul_string(raw_string)
+        # Parsing strings
+        for raw_string in mul_raw_strings:
+            valid: bool or list[int, int] = self._valid_mul_string(raw_string)
             if valid:
                 mul_numbers.append(valid)
 
