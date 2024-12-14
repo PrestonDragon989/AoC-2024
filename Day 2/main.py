54401d0d9b7b58d472e9ef91991b89b89820cc48
Preston E <122578013+PrestonDragon989@users.noreply.github.com>
2024-12-13 18:16:29 -0800

    Added part 2
    Added in part 2 basic logic to get correct answer, using same base logic as part 1 with minor tweaks.

--- Day 2/main.py
+++ Day 2/main.py
@@ -1,5 +1,6 @@
 """
 Main file, this is the file to run when you want to see day 2. This holds the 'main' function.
+URL: https://adventofcode.com/2024/day/2#part2
 """
 from parser import InputParser
 
@@ -13,6 +14,11 @@ def main() -> None:
         that's how they are linked. Then it checks if the report is either descending or ascending, and holds it to
         that, marking it unsafe it doesn't follow that behavior. It then checks total distance between the current
         number, and next number. If the gap is more than 3, or less than 1, marks it unsafe.
+
+    Part 2:
+        Checks to see if the report was safe before dampened, and if was, mark it as safe. If not, iterate through
+        the reports length and remove one item until gone through every bit of the list. If the report is valid (using
+        the part 1 method) mark it as valid and move one.
     """
     reports: list[list[int]] = InputParser("input.txt").get_parsed_lists()
 
@@ -38,6 +44,42 @@ def main() -> None:
     for i, report in enumerate(reports):
         print(f"{report} : {is_safe[i]}")
     print(f"Total Safe Reports: {total_safe_reports}")
+
+    # Entering part 2
+    print("\n|\n|\n| PART 2\n|\n|\n")
+
+    problem_dampener_safe: list[bool] = []
+    for j, report in enumerate(reports):
+        if is_safe[j]:
+            problem_dampener_safe.append(True)
+            continue
+
+        dampened_safe: bool = False
+        for i in range(len(report)):
+            dampened_report: list[int] = report.copy()
+            dampened_report.pop(i)
+
+            safe: bool = True
+            is_ascending: bool = dampened_report[0] - dampened_report[1] > 0
+            for num in range(len(dampened_report) - 1):
+                distance: int = dampened_report[num] - dampened_report[num + 1]  # Distance between i & neighbour i
+                if (is_ascending and distance > 0) or (not is_ascending and distance < 0):  # Ascend/Descend continuity
+                    safe = 1 <= abs(distance) <= 3
+                else:
+                    safe = False
+
+                if not safe:
+                    break
+
+            if safe:
+                dampened_safe = True
+                break
+
+        problem_dampener_safe.append(dampened_safe)
+
+    for i, report in enumerate(reports):
+        print(f"{report} : {problem_dampener_safe[i]}")
+    print(f"Total Safe Dampened Reports: {problem_dampener_safe.count(True)}")
 
 
 # Running Main function, as if this language was normal
