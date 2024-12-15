6087a22b42963da9ca5c1811865d4e5ee05a09af
Preston E <122578013+PrestonDragon989@users.noreply.github.com>
2024-12-14 22:15:36 -0800

    Completed Part 2
    Abstracted some bits of reusable code (parse mul strings, remove substring) and edited the valid_mul_string function to be more accurate, and simple. Added function in parser for Part 2, which got the proper parameters using new rules. Added in appropriate docstrings to everything.

--- Day 3/main.py
+++ Day 3/main.py
@@ -7,22 +7,36 @@ from parser import InputParser
 
 def main():
     """
-    Main function for the Day 3 AoC challenge. First collects the mul_values from the input file with InputParser.
+    Main function for the Day 3 AoC challenge. Init the InputParser.
 
     Part 1:
+        First collects the mul_values from the input file with InputParser. It doesn't check for 'do()' or 'don't()'.
         iterating through the mul values, then multiplying the two numbers together and adding them to a total.
-    """
-    mul_values: list[[int, int]] = InputParser("input.txt").get_mul_numbers()
 
-    print(mul_values)
-    print(len(mul_values))
+    Part 2:
+        It collects the mul parameters from the parser, this time checking for 'do()' and 'don't()'. Then it multiplies
+        them, and collects the total.
+    """
+    parser = InputParser('input.txt')
+    mul_values: list[[int, int]] = parser.get_mul_numbers()
 
     # Part 1
-    total_mul: float = 0
+    total_mul: int = 0
     for mul_parameters in mul_values:
         total_mul += mul_parameters[0] * mul_parameters[1]
 
     print(f"Total Mul: {total_mul}")
+
+    # Entering part 2
+    print("\n|\n|\n| PART 2\n|\n|\n")
+
+    do_check_values: list[[int, int]] = parser.get_do_check_mul_numbers()
+
+    total_do_checked_values: int = 0
+    for mul_parameters in do_check_values:
+        total_do_checked_values += mul_parameters[0] * mul_parameters[1]
+
+    print(f"Total Do Checked Mul: {total_do_checked_values}")
 
 
 if __name__ == '__main__':
